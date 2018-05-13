8d694ca92e04bf5aa2af3911826fd7f704f314e8
FoeX <me@codefox.cf>
2018-05-13 20:41:49 +0200

    Add files via upload

---
+++ install.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         steal my mic
+// @namespace    stealmic
+// @version      1.0
+// @description  steal my mic
+// @author       FoeX
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    new Audio('https://my.mixtape.moe/noyuoo.mp3').play()
+})();
